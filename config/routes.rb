11cd64072c2bb7726b3f34d2248204e3c7858dee
Layla <laylamandella@gmail.com>
2014-07-16 19:03:29 -0700

    Spring cleaning

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 CarbonQualms::Application.routes.draw do
-  get "home/index"
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'home#index'
+  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -14,7 +12,7 @@ CarbonQualms::Application.routes.draw do
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
-#  resources :bad_actions
+  #   resources :products
 
   # Example resource route with options:
   #   resources :products do
