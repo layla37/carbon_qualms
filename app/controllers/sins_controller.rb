608182a054d64ba735ec5e1b5ce5b2892b070128
Layla <laylamandella@gmail.com>
2014-07-16 21:11:54 -0700

    added links to edit eco-sins

--- app/controllers/sins_controller.rb
+++ app/controllers/sins_controller.rb
@@ -1,15 +1,33 @@
 class SinsController < ApplicationController
-	
+
 	# TODO this is a quick hack but I need to figure out how to 
 	# deal with the cookies and the secret token thing.
 	skip_before_action :verify_authenticity_token
+
+	def index
+		@sins = Sin.all
+	end
 
 	def create
 		b = Sin.new
 		b.description = params["description"]
 		b.footprint = params["footprint"]
 		b.save
-		@sins = Sin.all
 	end
+
+	def show
+  	end
+
+  	def new
+  	end
+
+    def edit
+  	end
+
+  	def update
+  	end
+
+  	def destroy
+  	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 CarbonQualms::Application.routes.draw do
   get "home/index"
   get "sins/new"
+  get "sins/edit"
   post "sins/create"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-root 'home#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
